ac917bceaa83427f8a23fb194270d900f2ccf5bf
Elidio <elidio289@gmail.com>
2025-03-17 12:37:57 -0300

    Fiz o AuthContext

---
+++ front/src/components/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false); // Simula o carregamento inicial, pode ser ajustado se for buscar do backend
+  }, []);
+
+  const login = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setToken(newToken);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
